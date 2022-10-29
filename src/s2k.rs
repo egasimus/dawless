9de3f7d09f6f012bf2064986a5851f3488981aed
Adam A <adam@hack.bg>
2022-10-29 23:19:19 +0300

    import passes cargo check, let's test it

--- src/s2k.rs
+++ src/s2k.rs
@@ -36,19 +36,25 @@ pub trait DeviceDisk<const M: DeviceModel, const N: usize> {
 
 impl DeviceDisk<{ DeviceModel::S900 }, 819200> for Device<{ DeviceModel::S900 }>  {
     fn create_blank_disk (&self) -> DiskImage<{ DeviceModel::S900 }, 819200> {
-        DiskImage { data: [0; 819200] }.blank()
+        let mut image = DiskImage { data: [0; 819200] };
+        image.blank();
+        image
     }
 }
 
 impl DeviceDisk<{ DeviceModel::S2000 }, 1638400> for Device<{ DeviceModel::S2000 }> {
     fn create_blank_disk (&self) -> DiskImage<{ DeviceModel::S2000 }, 1638400> {
-        DiskImage { data: [0; 1638400] }.blank()
+        let mut image = DiskImage { data: [0; 1638400] };
+        image.blank();
+        image
     }
 }
 
 impl DeviceDisk<{ DeviceModel::S3000 }, 1638400> for Device<{ DeviceModel::S3000 }> {
     fn create_blank_disk (&self) -> DiskImage<{ DeviceModel::S3000 }, 1638400> {
-        DiskImage { data: [0; 1638400] }.blank()
+        let mut image = DiskImage { data: [0; 1638400] };
+        image.blank();
+        image
     }
 }
 
@@ -65,40 +71,39 @@ pub trait DiskData<const N: usize>: Sized {
         index + length
     }
 
-    fn blank (&mut self) -> Self {
-        let index = 0;
-        let index = self.put_data(index, &self.section_dp1());
-        let index = self.put_data(index, &self.section_dp2());
-        let index = self.put_data(index, &self.section_dp3());
-        let index = self.put_data(index, &self.section_dp4());
-        let index = self.put_data(index, &self.section_dp5());
-        let index = self.put_data(index, &self.section_dp6());
-        *self
+    fn blank (&mut self) {
+        let index  = 0;
+        let index  = self.put_data(index, &self.section_dp1());
+        let index  = self.put_data(index, &self.section_dp2());
+        let index  = self.put_data(index, &self.section_dp3());
+        let index  = self.put_data(index, &self.section_dp4());
+        let index  = self.put_data(index, &self.section_dp5());
+        let _index = self.put_data(index, &self.section_dp6());
     }
 
     /** Disk header. */
-    fn section_dp1 (&mut self) -> [u8; 24];
+    fn section_dp1 (&self) -> [u8; 24];
 
     /** Bunch of empty space. (what does it mean?) */
-    fn section_dp2 (&mut self) -> [u8; 3166] {
+    fn section_dp2 (&self) -> [u8; 3166] {
         [0; 3166]
     }
 
     /** Volume name in AKAI format. (offset 4736) */
-    fn section_dp3 (&mut self) -> [u8; 12] {
+    fn section_dp3 (&self) -> [u8; 12] {
         [0x18, 0x19, 0x1E, 0x0A, 0x18, 0x0B, 0x17, 0x0F, 0x0E, 0x0A, 0x0A, 0x0A]
     }
 
     /** Not supported on S900, different on S2000 vs S3000. (what does it mean?) */
-    fn section_dp4 (&mut self) -> [u8; 372];
+    fn section_dp4 (&self) -> [u8; 372];
 
     /** 512 possible directory entries. (offset 5120) */
-    fn section_dp5 (&mut self) -> [u8; 12288] {
+    fn section_dp5 (&self) -> [u8; 12288] {
         [0; 12288]
     }
 
     /** 1583 * 1024 byte sectors for data. (offset 17408?) */
-    fn section_dp6 (&mut self) -> [u8; 1620992] {
+    fn section_dp6 (&self) -> [u8; 1620992] {
         [0; 1620992]
     }
 }
@@ -107,14 +112,14 @@ impl DiskData<819200> for DiskImage<{ DeviceModel::S900 }, 819200> {
     fn data_mut (&mut self) -> &mut [u8; 819200] {
         &mut self.data
     }
-    fn section_dp1 (&mut self) -> [u8; 24] {
+    fn section_dp1 (&self) -> [u8; 24] {
         [
             0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,
             0x0A, 0x0A, 0x0A, 0x0A, 0x00, 0x00, 0x06, 0x0A,
             0xFF, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00
         ]
     }
-    fn section_dp4 (&mut self) -> [u8; 372] {
+    fn section_dp4 (&self) -> [u8; 372] {
         unreachable!("S900 images have no dp4 section")
     }
 }
@@ -123,14 +128,14 @@ impl DiskData<1638400> for DiskImage<{ DeviceModel::S2000 }, 1638400> {
     fn data_mut (&mut self) -> &mut [u8; 1638400] {
         &mut self.data
     }
-    fn section_dp1 (&mut self) -> [u8; 24] {
+    fn section_dp1 (&self) -> [u8; 24] {
         [
             0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,
             0x0A, 0x0A, 0x0A, 0x0A, 0x00, 0x00, 0x06, 0x0A,
             0xFF, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x17
         ]
     }
-    fn section_dp4 (&mut self) -> [u8; 372] {
+    fn section_dp4 (&self) -> [u8; 372] {
         let mut data = [0; 372];
         const DP4_S2000: [u8; 14] = [
             0x00, 0x00, 0x00, 0x11, 0x00, 0x01, 0x01,
@@ -145,14 +150,14 @@ impl DiskData<1638400> for DiskImage<{ DeviceModel::S3000 }, 1638400> {
     fn data_mut (&mut self) -> &mut [u8; 1638400] {
         &mut self.data
     }
-    fn section_dp1 (&mut self) -> [u8; 24] {
+    fn section_dp1 (&self) -> [u8; 24] {
         [
             0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,
             0x0A, 0x0A, 0x0A, 0x0A, 0x00, 0x00, 0x06, 0x0A,
             0xFF, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x16
         ]
     }
-    fn section_dp4 (&mut self) -> [u8; 372] {
+    fn section_dp4 (&self) -> [u8; 372] {
         let mut data = [0; 372];
         const DP4_S3000: [u8; 14] = [
             0x00, 0x00, 0x32, 0x10, 0x00, 0x01, 0x01,
@@ -163,100 +168,115 @@ impl DiskData<1638400> for DiskImage<{ DeviceModel::S3000 }, 1638400> {
     }
 }
 
-fn import_image <const N: usize> (contents: &[u8; N]) {
+const BLOCK_SIZE: usize = 1024;
+const MAX_BLOCKS: usize = 1536;
+const FAT_HEADER: usize = 24;
 
-    let volname = &contents[4736..26];
+struct DiskFiles<const N: usize> {
+    model:  DeviceModel,
+    blocks: [[u8; BLOCK_SIZE]; MAX_BLOCKS],
+    head:   [[u8; FAT_HEADER]; N],
+    size:   [u32; N],
+    data:   [Vec<u8>; N],
+    free:   usize
+}
 
-    let model = match volname[23] {
-        0x00 => DeviceModel::S900,
-        0x17 => DeviceModel::S2000,
-        0x16 => DeviceModel::S3000,
-        _ => panic!("could not determine device model from image data")
-    };
+impl<const N: usize> DiskFiles<N> {
 
-    // Start of header
-    let startb  = match model { DeviceModel::S900 => 1536, _ => 1570 };
+    /** Create a filesystem. */
+    fn new (model: DeviceModel) -> Self {
+        Self {
+            model:  model,
+            blocks: [[0; BLOCK_SIZE]; MAX_BLOCKS],
+            head:   [[0; FAT_HEADER]; N],
+            size:   [0; N],
+            data:   array_init::array_init(|_| vec![]),
+            free:   0
+        }
+    }
 
-    // End of header
-    let endb    = match model { DeviceModel::S900 => 3136, _ => 3166 };
+    /** Create a filesystem and populate it with data from a disk image. */
+    fn load (model: DeviceModel, contents: &[u8]) -> Self {
+        let mut fs = Self::new(model);
+        fs.load_heads(contents);
+        fs.load_tails(contents);
+        fs
+    }
 
-    // Offset of file table
-    let offset  = match model { DeviceModel::S900 => 0,    _ => 5120 };
-
-    // Number of entries in file table
-    let entries = match model { DeviceModel::S900 => 64,   _ => 512  };
-
-    // Free block(s?)
-    let fb      = match model { DeviceModel::S900 => 796,  _ => 1583 };
-
-    let fat;
-    let file;
-    let freeblocks;
-    let fsize;
-    let ttype;
-    let fs;
-
-    if contents[offset] != 0x00 {
-
-        let mut cl    = 0;
-
-        // Block offset?
-        let mut block = 0;
-
-        let tmap = &contents[startb..endb-startb];
-
-        blocks = str_split($contents,1024);
-
-        for i in 0..entries {
-            let entry_offset = offset + i * 24;
-            if (contents[entry_offset] == 0x00) {
+    /// Reads the metadata and 1st block of each file in the disk image.
+    /// Corresponds to 1st loop of s2kdie importimage().
+    fn load_heads (&mut self, contents: &[u8]) -> &mut Self {
+        let (_, _, data_offset, max_entries, max_blocks) = get_model_parameters(&self.model);
+        // Used to determine number of remaining blocks
+        let mut last_block = 0;
+        // Read up to `max_entries` FS records
+        let mut i = 0;
+        while i < max_entries {
+            let entry_offset = data_offset + i * 24;
+            // If we've reached an empty entry we're past the end
+            if contents[entry_offset] == 0x00 {
                 break
             }
-
-            let fileentry = &contents[entry_offset..24];
-
-            fat[cl] = fileentry;
-
-            sb = hexdec(
-                str_pad(dechex(ord($fileentry[21])),2,"0",STR_PAD_LEFT) .
-                str_pad(dechex(ord($fileentry[20])),2,"0",STR_PAD_LEFT)
-            );
-
-            fsize[cl] = hexdec(
-                str_pad(dechex(ord($fileentry[19])),2,"0",STR_PAD_LEFT) . 
-                str_pad(dechex(ord($fileentry[18])),2,"0",STR_PAD_LEFT) . 
-                str_pad(dechex(ord($fileentry[17])),2,"0",STR_PAD_LEFT)
-            );
-
-            file[cl] = blocks[sb];
-  
-            block = block + fsize[cl] / 1024;
-
-            cl += 1;
+            let mut head: [u8; 24] = [0; 24];
+            head.copy_from_slice(&contents[entry_offset..24]);
+            self.head[i] = head;
+            self.size[i] = u32::from_le_bytes([head[17], head[18], head[19], 0x00]);
+            self.data[i] = self.blocks[u16::from_le_bytes([head[20], head[21]]) as usize].into();
+            last_block += self.size[i] / 1024;
+            i += 1;
         }
+        self.free = max_blocks - last_block as usize;
+        self
+    }
 
-        let mut bc = 0;
-        for i in [0..tmap.len()].iter().step_by(2) {
-          if bc >= sizeof($fat) {
-              continue
-          }
-          if tmap[i] == 0x00 && tmap[i+1] == 0x00 {
-            continue;
-          }
-          if (tmap[i] == 0x00 && tmap[i+1] == 0x80) || (tmap[i] == 0x00 && tmap[i+1] == 0xC0) {
-            bc += 1;
-          } else {
-            file[bc].append(
-                blocks[hexdec(str_pad(dechex(ord($tmap[$i+1])),2,"0",STR_PAD_LEFT) .
-                str_pad(dechex(ord($tmap[$i])),2,"0",STR_PAD_LEFT))]
-            );
-          }
+    /// Reads subsequent blocks (fragments) from the image.
+    /// Corresponds to 2nd loop of s2kdie importimage()
+    fn load_tails (&mut self, contents: &[u8]) -> &mut Self {
+        let (startb, endb, _, _, _) = get_model_parameters(&self.model);
+        // Map of fragments
+        let tmap = &contents[startb..endb-startb];
+        let mut block_count = 0;
+        for i in (0..tmap.len()).step_by(2) {
+            if tmap[i] == 0x00 && tmap[i+1] == 0x00 {
+                continue
+            }
+            if (tmap[i] == 0x00 && tmap[i+1] == 0x80) || (tmap[i] == 0x00 && tmap[i+1] == 0xC0) {
+                block_count += 1;
+            } else {
+                let block_index = u16::from_le_bytes([tmap[i], tmap[i+1]]);
+                let block_data  = &contents[block_index as usize..1024];
+                self.data[block_count].append(&mut block_data.to_vec());
+            }
         }
-        freeblocks = fb - block;
+        self
     }
 
 }
 
+fn guess_model (contents: &[u8]) -> DeviceModel {
+    let volname = &contents[4736..26];
+    match volname[23] {
+        0x00 => DeviceModel::S900,
+        0x17 => DeviceModel::S2000,
+        0x16 => DeviceModel::S3000,
+        _ => panic!("could not determine device model from image data")
+    }
+}
+
+fn get_model_parameters (model: &DeviceModel) -> (usize, usize, usize, usize, usize) {
+    // Start of header
+    let startb  = match model { DeviceModel::S900 => 1536, _ => 1570 };
+    // End of header
+    let endb    = match model { DeviceModel::S900 => 3136, _ => 3166 };
+    // Offset of file table
+    let offset  = match model { DeviceModel::S900 => 0,    _ => 5120 };
+    // Number of entries in file table
+    let entries = match model { DeviceModel::S900 => 64,   _ => 512  };
+    // Free block(s?)
+    let fb      = match model { DeviceModel::S900 => 796,  _ => 1583 };
+
+    (startb, endb, offset, entries, fb)
+}
 
 pub enum DataTypes {
     Program = 240,
@@ -266,7 +286,3 @@ pub enum DataTypes {
     OS      = 99,
     Deleted = 0
 }
-
-pub fn ascii2akai (inp: Vec<u8>) -> Vec<u8> {}
-
-pub fn akai2ascii (inp: vec<u8>) -> Vec<u8> {}
