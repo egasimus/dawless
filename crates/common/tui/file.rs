096b2b6d0f6031ce7e63001060173cdf6a655d57
Adam A <adam@hack.bg>
2022-12-09 22:23:33 +0200

    FileList wraps List

--- crates/common/tui/file.rs
+++ crates/common/tui/file.rs
@@ -1,25 +1,26 @@
 use super::*;
 
-pub fn render_directory_listing (
-    term: &mut dyn Write, col1: u16, row1: u16, pad: usize,
-    items: &Vec<(String, (String, bool))>,
-    selected: usize,
-) -> Result<()> {
-    let bg = Color::AnsiValue(232);
-    let fg = Color::White;
-    let hi = Color::Yellow;
-    for (index, (_, (path, is_dir))) in items.iter().enumerate() {
-        term.queue(SetAttribute(if *is_dir { Attribute::Bold } else { Attribute::Reset }))?
-            .queue(SetBackgroundColor(bg))?
-            .queue(SetForegroundColor(if selected == index { hi } else { fg }))?
-            .queue(MoveTo(col1, row1 + index as u16))?
-            .queue(Print(format!("{} {:<0width$}",
-                if *is_dir { "■" } else { " " },
-                path,
-                width = pad as usize
-            )))?;
-    }
-    Ok(())
-}
+pub type FileListItem = (String, bool);
 
-pub struct FileList (List<(String, bool)>);
+pub struct FileList<'a> (pub &'a List<FileListItem>);
+
+impl<'a> TUI for FileList<'a> {
+
+    fn render (&self, term: &mut dyn Write) -> Result<()> {
+        let Theme { bg, fg, hi } = self.0.theme;
+        let (col1, row1, cols, ..) = self.0.rect;
+        for (index, (_, (path, is_dir))) in self.0.items.iter().enumerate() {
+            term.queue(SetAttribute(if *is_dir { Attribute::Bold } else { Attribute::Reset }))?
+                .queue(SetBackgroundColor(bg))?
+                .queue(SetForegroundColor(if self.0.index == index { hi } else { fg }))?
+                .queue(MoveTo(col1, row1 + index as u16))?
+                .queue(Print(format!("{} {:<0width$}",
+                    if *is_dir { "■" } else { " " },
+                    path,
+                    width = cols as usize
+                )))?;
+        }
+        Ok(())
+    }
+
+}
